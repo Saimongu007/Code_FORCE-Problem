513d18a55a2b46a2bbe787e1ce79961928148cff
Saimongu Marma <124234753+Saimongu007@users.noreply.github.com>
2023-06-17 23:39:43 +0600

    Basic Data Types

---
+++ Basic Data Types cf.cpp
@@ -0,0 +1,15 @@
+//basic data type
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i;
+    long long lng;
+    char ch;
+    float f;
+    double d;
+
+    cin>>i>>lng>>ch>>f>>d;
+
+    cout<<i<<endl<<lng<<endl<<ch<<endl<<f<<endl<<d<<endl;
+}
