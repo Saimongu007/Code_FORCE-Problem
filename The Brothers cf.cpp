cb66789d8793480dad4ebd95845c389d2ee1d5fc
Saimongu Marma <124234753+Saimongu007@users.noreply.github.com>
2023-06-20 03:14:26 +0600

    The Brothers cf
    1st last string equal 2nd last string check

---
+++ The Brothers cf.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string f1,f2,l1,l2;
+    cin>>f1>>l1;
+    cin>>f2>>l2;
+    if(l1 == l2)
+    {
+        cout<<"ARE Brothers"<<endl;
+    }
+    else
+    {
+        cout<<"NOT";
+    }
+    return 0;
+}
+/*#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string f1,f2,s1,s2;
+    cin>>f1>>s1;
+    cin>>f1>>s2;
+    if(s1==s2)
+    {
+        cout<<"ARE Brothers"<<endl;
+    }
+    else
+    {
+        cout<<"NOT"<<endl;
+    }
+}*/
