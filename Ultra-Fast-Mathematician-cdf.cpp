df90f134f1bfa0bb62177d376fcddc23a64dd791
Saimongu Marma <124234753+Saimongu007@users.noreply.github.com>
2023-12-16 01:43:53 +0600

    (A)--See if you are faster and more accurate.

---
+++ Ultra-Fast-Mathematician-cdf.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string a, b, c;
+    cin >> a >> b;
+
+    for (int i = 0; i < a.size(); i++)
+    {
+        if (a[i] == b[i])
+        {
+            c = c + '0';
+        }
+        else
+        {
+            c = c + '1';
+        }
+    }
+    cout << c << endl;
+
+    return 0;
+}
