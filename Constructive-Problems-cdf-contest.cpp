c458d0bbb151f1732e42e0108abeafe864d88dde
Saimongu Marma <124234753+Saimongu007@users.noreply.github.com>
2024-01-06 21:42:54 +0600

    https://codeforces.com/problemset/problem/1905/A

---
+++ Constructive-Problems-cdf-contest.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n, m;
+        cin >> n >> m;
+
+        int r = max(n, m);
+        cout << r << endl;
+    }
+
+    return 0;
+}
